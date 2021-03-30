828146584131135ee034cb77e762b83595338029
GabrielRF1 <rasferreiramilde@mail.com>
2021-03-29 22:23:54 -0300

    getResVal

--- SimulationScenario.cpp
+++ SimulationScenario.cpp
@@ -18,91 +18,91 @@ SimulationScenario::SimulationScenario() {
 }
 
 bool SimulationScenario::startSimulation(std::string* errorMessage) {
-	// model->loadmodel _modelFilename
-	// set values for the _selectedControls
-	// model->startSimulation
-	// get the value of the _selectedResponses from the model and store results on _responseValues
-	// clear selected controls and responses
-	// close the model
-    
+    // model->loadmodel _modelFilename
+    // set values for the _selectedControls
+    // model->startSimulation
+    // get the value of the _selectedResponses from the model and store results on _responseValues
+    // clear selected controls and responses
+    // close the model
+
     // Creating and loading model
     Simulator * simulator = new Simulator();
     Model * model = new Model(simulator);
     model->load(this->_modelFilename);
-    
+
     // Setting control values
     for (
             std::list<std::pair<std::string, double>*>::iterator scen_it = this->_selectedControls->begin();
             scen_it != this->_selectedControls->end();
             ++scen_it
-        ) 
-    {
+            ) {
         for (
                 std::list<SimulationControl*>::iterator mod_it = model->getControls()->list()->begin();
                 mod_it != model->getControls()->list()->end();
                 ++mod_it
-            )
-        {
+                ) {
             if (!(*scen_it)->first.compare((*mod_it)->getName())) {
                 (*mod_it)->setValue((*scen_it)->second);
             }
         }
     }
-    
+
     // Running simulation
     model->getSimulation()->start();
-    
+
     // Acquiring response values
     for (
             std::list<std::string>::iterator scen_it = this->_selectedResponses->begin();
             scen_it != this->_selectedResponses->end();
             ++scen_it
-        ) 
-    {
+            ) {
         for (
                 std::list<SimulationResponse*>::iterator mod_it = model->getResponses()->list()->begin();
                 mod_it != model->getResponses()->list()->end();
                 ++mod_it
-            )
-        {
+                ) {
             if (!(*scen_it).compare((*mod_it)->getName()))
                 this->_responseValues->push_back(new std::pair<std::string, double>((*scen_it), (*mod_it)->getValue()));
         }
     }
-    
+
     model->clear();
     delete model;
     return true;
 }
 
 void SimulationScenario::setScenarioName(std::string _name) {
-	this->_scenarioName = _name;
+    this->_scenarioName = _name;
 }
 
 std::string SimulationScenario::getScenarioName() const {
-	return _scenarioName;
+    return _scenarioName;
+}
+
+std::list<std::pair<std::string, double>*>* SimulationScenario::getResponseValues() {
+    return _responseValues;
 }
 
 void SimulationScenario::setModelFilename(std::string _modelFilename) {
-	this->_modelFilename = _modelFilename;
+    this->_modelFilename = _modelFilename;
 }
 
 std::string SimulationScenario::getModelFilename() const {
-	return _modelFilename;
+    return _modelFilename;
 }
 
 std::list<std::string>* SimulationScenario::getSelectedResponses() const {
-	return _selectedResponses;
+    return _selectedResponses;
 }
 
 std::list<std::pair<std::string, double>*>* SimulationScenario::getSelectedControls() const {
-	return _selectedControls;
+    return _selectedControls;
 }
 
 void SimulationScenario::setScenarioDescription(std::string _scenarioDescription) {
-	this->_scenarioDescription = _scenarioDescription;
+    this->_scenarioDescription = _scenarioDescription;
 }
 
 std::string SimulationScenario::getScenarioDescription() const {
-	return _scenarioDescription;
+    return _scenarioDescription;
 }
