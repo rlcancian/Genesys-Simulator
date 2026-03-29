04b4c57eafdffb66718eca4afea423b92e6b1f03
Prof. Rafael Cancian, Dr. Eng. <cancian@lisha.ufsc.br>
2026-03-29 10:31:34 -0300

    test: add link shim for experiment manager support tests

--- source/tests/unit/test_simulator_support.cpp
+++ source/tests/unit/test_simulator_support.cpp
@@ -3,6 +3,17 @@
 #include "kernel/simulator/TraceManager.h"
 #include "kernel/simulator/ParserManager.h"
 #include "kernel/simulator/LicenceManager.h"
+#include "kernel/simulator/ExperimentManager.h"
+#include "kernel/simulator/Simulator.h"
+
+
+// Test-only link shim:
+// ExperimentManager.cpp references Simulator::getTraceManager() const
+// in methods that are not exercised by this unit test target.
+TraceManager* Simulator::getTraceManager() const {
+    return nullptr;
+}
+
 
 TEST(SimulatorSupportTest, TraceManagerInitializesErrorMessagesList) {
     TraceManager tm(nullptr);
@@ -23,3 +34,16 @@ TEST(SimulatorSupportTest, DefaultActivationCodeReportsNotFound) {
     LicenceManager lm(nullptr);
     EXPECT_EQ(lm.showActivationCode(), "ACTIVATION CODE: Not found.");
 }
+
+TEST(SimulatorSupportTest, ExperimentManagerStartsWithoutCurrentExperiment) {
+    ExperimentManager em(nullptr);
+    EXPECT_EQ(em.current(), nullptr);
+}
+
+TEST(SimulatorSupportTest, NewSimulationExperimentBecomesCurrent) {
+    ExperimentManager em(nullptr);
+    SimulationExperiment* exp = em.newSimulationExperiment();
+    ASSERT_NE(exp, nullptr);
+    EXPECT_EQ(em.current(), exp);
+}
+
