bd6672fcd589ff63666d1f8d2523844d40499b7f
Prof. Rafael Cancian, Dr. Eng. <cancian@lisha.ufsc.br>
2026-03-30 15:50:05 -0300

    Fix probability distribution logic and harden hypothesis tester stubs

--- source/tools/HypothesisTesterDefaultImpl1.cpp
+++ source/tools/HypothesisTesterDefaultImpl1.cpp
@@ -14,7 +14,24 @@
 #include "ProbabilityDistribution.h"
 #include "../kernel/statistics/StatisticsDataFile_if.h"
 #include "../kernel/TraitsKernel.h"
-#include <math.h>
+#include <algorithm>
+#include <cmath>
+#include <limits>
+#include <memory>
+#include <stdexcept>
+#include <string_view>
+
+namespace {
+
+[[noreturn]] void throwUnimplemented(std::string_view methodName) {
+	throw std::logic_error("Method not implemented yet: " + std::string(methodName));
+}
+
+double clampProbability(double value) {
+	return std::clamp(value, 0.0, 1.0);
+}
+
+}
 
 HypothesisTesterDefaultImpl1::HypothesisTesterDefaultImpl1() {
 }
@@ -43,8 +60,9 @@ HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::proportion
 }
 
 HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::varianceConfidenceInterval(double var, unsigned int n, double confidenceLevel) {
-	double il = (n - 1) * var / ProbabilityDistribution::inverseChi2((1.0 - confidenceLevel) / 2.0, n - 1);
-	double sl = (n - 1) * var / ProbabilityDistribution::inverseChi2(confidenceLevel / 2.0, n - 1);
+	const double alpha = 1.0 - confidenceLevel;
+	double il = (n - 1) * var / ProbabilityDistribution::inverseChi2(1.0 - alpha / 2.0, n - 1);
+	double sl = (n - 1) * var / ProbabilityDistribution::inverseChi2(alpha / 2.0, n - 1);
 	double e0 = (sl - il) / 2.0;
 	return HypothesisTester_if::ConfidenceInterval(il, sl, e0);
 }
@@ -64,7 +82,15 @@ HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::averageDif
 }
 
 HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::proportionDifferenceConfidenceInterval(double avg1, double stddev1, unsigned int n1, double avg2, double stddev2, unsigned int n2, double confidenceLevel) {
-
+	constexpr auto epsilon = std::numeric_limits<double>::epsilon();
+	const double denominator = std::sqrt((stddev1 * stddev1) / n1 + (stddev2 * stddev2) / n2);
+	if (denominator <= epsilon) {
+		return HypothesisTester_if::ConfidenceInterval(avg1 - avg2, avg1 - avg2, 0.0);
+	}
+	double correctConf = (1.0 - confidenceLevel) / 2.0;
+	double critic = -ProbabilityDistribution::inverseTStudent(correctConf, 0.0, 1.0, std::min(n1, n2) - 1);
+	double e0 = critic * denominator;
+	return HypothesisTester_if::ConfidenceInterval(avg1 - avg2 - e0, avg1 - avg2 + e0, e0);
 }
 
 HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::varianceRatioConfidenceInterval(double var1, unsigned int n1, double var2, unsigned int n2, double confidenceLevel) {
@@ -81,13 +107,13 @@ HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::varianceRa
 // confidence intervals based on datafile
 
 HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::averageConfidenceInterval(std::string sampleDataFilename, double confidenceLevel) {
-	StatisticsDatafile_if* stat = new TraitsKernel<StatisticsDatafile_if>::Implementation();
+	auto stat = std::make_unique<TraitsKernel<StatisticsDatafile_if>::Implementation>();
 	static_cast<CollectorDatafile_if*> (stat->getCollector())->setDataFilename(sampleDataFilename);
 	return averageConfidenceInterval(stat->average(), stat->stddeviation(), stat->numElements(), confidenceLevel);
 }
 
 HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::proportionConfidenceInterval(std::string sampleDataFilename, checkProportionFunction function, double confidenceLevel) {
-	StatisticsDatafile_if* stat = new TraitsKernel<StatisticsDatafile_if>::Implementation();
+	auto stat = std::make_unique<TraitsKernel<StatisticsDatafile_if>::Implementation>();
 	static_cast<CollectorDatafile_if*> (stat->getCollector())->setDataFilename(sampleDataFilename);
 	unsigned long count = 0;
 	double value;
@@ -101,11 +127,21 @@ HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::proportion
 }
 
 HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::proportionConfidenceInterval(std::string sampleDataFilename, checkProportionFunction function, double N, double confidenceLevel) {
-
+	auto stat = std::make_unique<TraitsKernel<StatisticsDatafile_if>::Implementation>();
+	static_cast<CollectorDatafile_if*> (stat->getCollector())->setDataFilename(sampleDataFilename);
+	unsigned long count = 0;
+	for (unsigned long i = 0; i < stat->numElements(); i++) {
+		const double value = static_cast<CollectorDatafile_if*> (stat->getCollector())->getValue(i);
+		if (function(value)) {
+			++count;
+		}
+	}
+	double prop = static_cast<double> (count) / stat->numElements();
+	return proportionConfidenceInterval(prop, stat->numElements(), static_cast<int> (N), confidenceLevel);
 }
 
 HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::varianceConfidenceInterval(std::string sampleDataFilename, double confidenceLevel) {
-	StatisticsDatafile_if* stat = new TraitsKernel<StatisticsDatafile_if>::Implementation();
+	auto stat = std::make_unique<TraitsKernel<StatisticsDatafile_if>::Implementation>();
 	static_cast<CollectorDatafile_if*> (stat->getCollector())->setDataFilename(sampleDataFilename);
 	return varianceConfidenceInterval(stat->variance(), stat->numElements(), confidenceLevel);
 }
@@ -113,7 +149,14 @@ HypothesisTester_if::ConfidenceInterval HypothesisTesterDefaultImpl1::varianceCo
 // estimate sample size
 
 unsigned int HypothesisTesterDefaultImpl1::estimateSampleSize(double avg, double stddev, double desiredE0, double confidenceLevel) {
-
+	(void) avg;
+	if (desiredE0 <= 0.0) {
+		return 0;
+	}
+	const double alphaHalf = (1.0 - confidenceLevel) / 2.0;
+	const double z = -ProbabilityDistribution::inverseNormal(alphaHalf, 0.0, 1.0);
+	const double n = std::pow((z * stddev) / desiredE0, 2.0);
+	return static_cast<unsigned int> (std::ceil(std::max(n, 1.0)));
 }
 
 
@@ -122,76 +165,114 @@ unsigned int HypothesisTesterDefaultImpl1::estimateSampleSize(double avg, double
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testAverage(double avg, double stddev, unsigned int n, double avgSample, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
 	double significanceLevel = (1.0 - confidenceLevel);
-	double cumulative;
-	if (comp == HypothesisTester_if::H1Comparition::LESS_THAN) {
-		cumulative = significanceLevel;
-	} else if (comp == HypothesisTester_if::H1Comparition::GREATER_THAN) {
-		cumulative = confidenceLevel;
-	} else {
-		cumulative = confidenceLevel + significanceLevel / 2.0;
-	}
-	double acceptInfLimit = ProbabilityDistribution::inverseTStudent(cumulative, 0.0, 1.0, n - 1);
-	double acceptSupLimit = ProbabilityDistribution::inverseTStudent(cumulative, 0.0, 1.0, n - 1);
+	double acceptInfLimit = ProbabilityDistribution::inverseTStudent((comp == HypothesisTester_if::H1Comparition::GREATER_THAN) ? significanceLevel : significanceLevel / 2.0, 0.0, 1.0, n - 1);
+	double acceptSupLimit = ProbabilityDistribution::inverseTStudent((comp == HypothesisTester_if::H1Comparition::LESS_THAN) ? confidenceLevel : 1.0 - significanceLevel / 2.0, 0.0, 1.0, n - 1);
 	double testStat = (avgSample - avg) / (stddev / sqrt(n));
-	double pvalue = ProbabilityDistribution::tStudent(avgSample, avg, stddev, n - 1);
-	return HypothesisTester_if::TestResult(pvalue, pvalue<significanceLevel, acceptInfLimit, acceptSupLimit, testStat);
+	const double oneTail = 0.5 * std::erfc(std::abs(testStat) / std::sqrt(2.0)); // normal approximation
+	double pvalue = (comp == HypothesisTester_if::H1Comparition::DIFFERENT) ? 2.0 * oneTail : oneTail;
+	return HypothesisTester_if::TestResult(clampProbability(pvalue), pvalue < significanceLevel, acceptInfLimit, acceptSupLimit, testStat);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testProportion(double prop, unsigned int n, double proptest, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) prop;
+	(void) n;
+	(void) proptest;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testVariance(double var, unsigned int n, double vartest, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) var;
+	(void) n;
+	(void) vartest;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 // two populations
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testAverage(double avg1, double stddev1, unsigned int n1, double avg2, double stddev2, unsigned int n2, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) avg1;
+	(void) stddev1;
+	(void) n1;
+	(void) avg2;
+	(void) stddev2;
+	(void) n2;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testProportion(double prop1, unsigned int n1, double prop2, unsigned int n2, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) prop1;
+	(void) n1;
+	(void) prop2;
+	(void) n2;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testVariance(double var1, unsigned int n1, double var2, unsigned int n2, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) var1;
+	(void) n1;
+	(void) var2;
+	(void) n2;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 // one population based on datafile
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testAverage(std::string sampleDataFilename, double avgSample, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) sampleDataFilename;
+	(void) avgSample;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testProportion(std::string sampleDataFilename, checkProportionFunction function, double proptest, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) sampleDataFilename;
+	(void) function;
+	(void) proptest;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testVariance(std::string sampleDataFilename, double vartest, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) sampleDataFilename;
+	(void) vartest;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 // two populations based on datafile
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testAverage(std::string firstSampleDataFilename, std::string secondSampleDataFilename, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) firstSampleDataFilename;
+	(void) secondSampleDataFilename;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testProportion(std::string firstSampleDataFilename, std::string secondSampleDataFilename, checkProportionFunction function, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) firstSampleDataFilename;
+	(void) secondSampleDataFilename;
+	(void) function;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 
 HypothesisTester_if::TestResult HypothesisTesterDefaultImpl1::testVariance(std::string firstSampleDataFilename, std::string secondSampleDataFilename, double confidenceLevel, HypothesisTester_if::H1Comparition comp) {
-	double pValue, rejectH0, acceptInfLim, acceptSupLim, testStat;
-	return HypothesisTester_if::TestResult(pValue, rejectH0, acceptInfLim, acceptSupLim, testStat); ///@TODO: not implemented yet
+	(void) firstSampleDataFilename;
+	(void) secondSampleDataFilename;
+	(void) confidenceLevel;
+	(void) comp;
+	throwUnimplemented(__func__);
 }
 // @TODO: Add interface for non-parametrical tests, such as chi-square (based on values and on datafile)
