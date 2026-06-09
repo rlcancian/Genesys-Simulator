44a51ced8aa826ebaf57ef28f48aee1e0a4695ba
heloisajh <heloisa.hammes@bridge.ufsc.br>
2026-06-09 11:00:52 -0300

    test: add menu interativo para rodar caminho feliz e teste de estresse

--- source/applications/terminal/examples/smarts/Smart_PetriPlace.cpp
+++ source/applications/terminal/examples/smarts/Smart_PetriPlace.cpp
@@ -1,5 +1,7 @@
 #include "Smart_PetriPlace.h"
 
+#include <iostream> // Necessário para o std::cin e std::cout
+
 #include "kernel/simulator/Simulator.h"
 #include "plugins/components/Logic/Create.h"
 #include "plugins/components/Logic/Dispose.h"
@@ -12,6 +14,13 @@ Smart_PetriPlace::Smart_PetriPlace() {
 }
 
 int Smart_PetriPlace::main(int argc, char** argv) {
+
+    int escolha;
+    std::cout << "1 - Teste de Caminho Feliz\n";
+    std::cout << "2 - Teste com Modal Vazio\n";
+    std::cout << "Digite o numero do teste: ";
+    std::cin >> escolha;
+
     Simulator* genesys = new Simulator();
     genesys->getTraceManager()->setTraceLevel(TraitsApp<GenesysApplication_if>::traceLevel);
     setDefaultTraceHandlers(genesys->getTraceManager());
@@ -22,44 +31,69 @@ int Smart_PetriPlace::main(int argc, char** argv) {
     Create* create = plugins->newInstance<Create>(model);
     Dispose* dispose = plugins->newInstance<Dispose>(model);
 
+    if (escolha == 1) {
 
-    ModalModelPetriNet* modal_vazio = new ModalModelPetriNet(model, "Modal_Sem_Nos");
+        ModalModelPetriNet* modal = new ModalModelPetriNet(model, "SubModeloPetriColorida");
 
-    ModalModelPetriNet* modal = new ModalModelPetriNet(model, "SubModeloPetriColorida");
+        PetriPlace* p1_azul = new PetriPlace(model, "Lugar1_Azul");
+        PetriPlace* p2_vermelho = new PetriPlace(model, "Lugar2_Vermelho");
+        PetriPlace* p3_roxo = new PetriPlace(model, "Lugar3_Roxo");
 
-    PetriPlace* p1_azul = new PetriPlace(model, "Lugar1_Azul");
-    PetriPlace* p2_vermelho = new PetriPlace(model, "Lugar2_Vermelho");
-    PetriPlace* p3_roxo = new PetriPlace(model, "Lugar3_Roxo");
+        p1_azul->setInitialNode(true);
+        p2_vermelho->setInitialNode(true);
 
-    p1_azul->setInitialNode(true);
-    p1_azul->addTokens(2, "blue");
+        p1_azul->addTokens(2, "blue");
+        p2_vermelho->addTokens(1, "red");
 
-    modal->addNode(p1_azul);
-    modal->addNode(p2_vermelho);
-    modal->addNode(p3_roxo);
-    modal->setEntryNode(p1_azul);
+        modal->addNode(p1_azul);
+        modal->addNode(p2_vermelho);
+        modal->addNode(p3_roxo);
 
-    PetriTransition* t_isolada = new PetriTransition(p1_azul, p3_roxo, "Trans_Isolada");
-    modal->addTransition(t_isolada);
+        modal->setEntryNode(p1_azul);
 
-    PetriTransition* t_erro_in = new PetriTransition(p1_azul, p3_roxo, "Trans_Erro_Entrada");
-    t_erro_in->setInputArcWeight(nullptr, "blue", 1);
-    t_erro_in->setInputArcWeight(p1_azul, "", 1);
-    t_erro_in->setInputArcWeight(p2_vermelho, "red", 0);
-    modal->addTransition(t_erro_in);
+        PetriTransition* t1_mistura = new PetriTransition(p1_azul, p3_roxo, "TransicaoMistura");
 
-    PetriTransition* t_erro_out = new PetriTransition(p1_azul, p3_roxo, "Trans_Erro_Saida");
-    t_erro_out->setOutputArcWeight(nullptr, "purple", 1);
-    t_erro_out->setOutputArcWeight(p3_roxo, "", 1);
-    t_erro_out->setOutputArcWeight(p3_roxo, "purple", 0);
-    modal->addTransition(t_erro_out);
+        t1_mistura->setInputArcWeight(p1_azul, "blue", 1);
+        t1_mistura->setInputArcWeight(p2_vermelho, "red", 1);
 
-    create->getConnectionManager()->insert(modal);
-    modal->getConnectionManager()->insert(dispose);
+        t1_mistura->setOutputArcWeight(p3_roxo, "purple", 1);
+
+        modal->addTransition(t1_mistura);
+
+        create->getConnectionManager()->insert(modal);
+        modal->getConnectionManager()->insert(dispose);
+
+    } else {
+
+        ModalModelPetriNet* modal_vazio = new ModalModelPetriNet(model, "Modal_Sem_Nos");
+        ModalModelPetriNet* modal = new ModalModelPetriNet(model, "SubModeloPetriColorida");
+
+        PetriPlace* p1_azul = new PetriPlace(model, "Lugar1_Azul");
+        PetriPlace* p2_vermelho = new PetriPlace(model, "Lugar2_Vermelho");
+        PetriPlace* p3_roxo = new PetriPlace(model, "Lugar3_Roxo");
+
+        p1_azul->setInitialNode(true);
+        modal->addNode(p1_azul);
+        modal->addNode(p2_vermelho);
+        modal->addNode(p3_roxo);
+        modal->setEntryNode(p1_azul);
+
+        PetriTransition* t_isolada = new PetriTransition(p1_azul, p3_roxo, "trans_isolada");
+        modal->addTransition(t_isolada);
+
+        PetriTransition* t_erro_in = new PetriTransition(p1_azul, p3_roxo, "trans_erro_entrada");
+        t_erro_in->setInputArcWeight(nullptr, "blue", 1);
+        t_erro_in->setInputArcWeight(p1_azul, "", 1);
+        t_erro_in->setInputArcWeight(p2_vermelho, "red", 0);
+        modal->addTransition(t_erro_in);
+
+        create->getConnectionManager()->insert(modal_vazio);
+        modal_vazio->getConnectionManager()->insert(modal);
+        modal->getConnectionManager()->insert(dispose);
+    }
 
     model->getSimulation()->setReplicationLength(10, Util::TimeUnit::second);
     model->save("./models/Smart_PetriPlace.gen");
-
     model->getSimulation()->start();
 
     delete genesys;
