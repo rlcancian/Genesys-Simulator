f98a3ea3a05305a891a1993d726d983a5d51d7af
oasjs <oasjs@proton.me>
2026-07-06 18:46:46 -0300

    fix: clean up dlerror before calling dlopen

--- source/plugins/StaticCppCompiler.cpp
+++ source/plugins/StaticCppCompiler.cpp
@@ -1,4 +1,5 @@
 #include "StaticCppCompiler.h"
+#include <dlfcn.h>
 
 StaticCppCompilerError::StaticCppCompilerError(const char *msg)
     : message(msg) {}
@@ -8,6 +9,7 @@ const char *StaticCppCompilerError::what() const noexcept {
 }
 
 void* StaticCppCompiler::loadLibrary(const char *path, int flags) {
+    dlerror();
     void *dynamicLibraryHandle = dlopen(path, flags);
     const char *error = dlerror();
     if (error != nullptr) {
