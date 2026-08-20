b9daa0456aa26b17c7e8e79f09493fb1bf2b9de6
Prof. Rafael Cancian, Dr. Ing. <cancian@lisha.ufsc.br>
2026-08-20 13:27:14 -0300

    launcher: make process launching injectable

--- source/applications/launcher/ProcessLauncher.h
+++ source/applications/launcher/ProcessLauncher.h
@@ -9,13 +9,22 @@ namespace genesys::launcher {
 
 class LauncherLogger;
 
-class ProcessLauncher {
+class IProcessLauncher {
+public:
+    virtual ~IProcessLauncher() = default;
+
+    [[nodiscard]] virtual int launchReplacingProcess(const RuntimeSelection& selection,
+                                                     const QStringList& arguments,
+                                                     QString* error = nullptr) = 0;
+};
+
+class ProcessLauncher final : public IProcessLauncher {
 public:
     explicit ProcessLauncher(LauncherLogger* logger = nullptr);
 
     [[nodiscard]] int launchReplacingProcess(const RuntimeSelection& selection,
                                              const QStringList& arguments,
-                                             QString* error = nullptr) const;
+                                             QString* error = nullptr) override;
 
 private:
     LauncherLogger* logger_ = nullptr;
